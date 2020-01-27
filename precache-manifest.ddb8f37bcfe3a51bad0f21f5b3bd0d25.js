d26bcd0969d63c73c9e4bd3cac5bd07938250633
lokutech <onatim@protonmail.com>
2020-01-27 20:31:38 +0300

    Updates

---
+++ precache-manifest.ddb8f37bcfe3a51bad0f21f5b3bd0d25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf1848e284ac93b56a86671da81069a",
+    "url": "/tajweed-quran/index.html"
+  },
+  {
+    "revision": "e4d8560dc4ba43cf8f32",
+    "url": "/tajweed-quran/static/css/main.9833ea78.chunk.css"
+  },
+  {
+    "revision": "9ff9431707612611c68d",
+    "url": "/tajweed-quran/static/js/2.0e330de9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tajweed-quran/static/js/2.0e330de9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4d8560dc4ba43cf8f32",
+    "url": "/tajweed-quran/static/js/main.b2ae91e2.chunk.js"
+  },
+  {
+    "revision": "d4a565a6c5814d792fe9",
+    "url": "/tajweed-quran/static/js/runtime-main.f38cb8ac.js"
+  },
+  {
+    "revision": "a1ffd466ac6f924bff676fe0e828c4cf",
+    "url": "/tajweed-quran/static/media/UthmanicHafs1Ex1Ver12.a1ffd466.otf"
+  }
+]);
