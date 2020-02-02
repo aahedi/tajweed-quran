0896a2de3ef35102ac720ec271acaad7d023de0e
lokutech <onatim@protonmail.com>
2020-02-02 17:01:07 +0300

    Madd complete!

--- src/App.js
+++ src/App.js
@@ -1,102 +1,111 @@
 import React from 'react'
 import './App.css'
 import ayahobject from './ayahobject.json'
+import tajweed from './tajweed'
 
 class App extends React.Component {
   state = {
     uthmani: '',
-    surah: '',
+    surahName: '',
     color: '',
-    ayahNumber: 1
+    ayahNumber: 1,
+    surahNumber: 1,
+    ayahNumberInSurah: 1
   }
 
   handleClick = () => {
-    this.setState({ ayahNumber: this.state.ayahNumber + 1 }, () =>
-      this.apiCall()
-    )
+    if (getAyah(1,10)){
+
+      this.setState({ ayahNumber: Number(this.state.ayahNumber) + 1 }
+      , () =>
+      this.apiCallNext()
+      )
+    }
   }
 
-  // goto = (e) => {
-  //   this.setState ( {ayahNumber: e.event.value})
-  // }
+  goto = (e) => {
+    e.preventDefault();
+    // let gotoNumber = e.target[0].value
+    this.setState({ayahNumber: this.state.ayahNumber}, () => this.apiCall())
+  }
+
+  handleChange = (e) => {
+    this.setState({ayahNumberInSurah : e.target.value})
+  }
+
+  handleChangeSurah = (e) => {
+    this.setState({surahNumber : e.target.value})
+  }
 
   componentDidMount = () => {
     this.apiCall()
   }
 
-  apiCall = () => {
+  apiCallNext = () => {
     let currentAyah = this.state.ayahNumber
     this.setState({
       uthmani:
         ayahobject[currentAyah].uthmaniText +
         ' ' +
         ayahobject[currentAyah].numberInSurahNativeReverse,
-      surah: ayahobject[currentAyah].surah.name
+      surahName: ayahobject[currentAyah].surah.name
     })
   }
 
+  apiCall = () => {
+    let currentAyah = this.state.ayahNumber
+    this.setState({
+      uthmani: getAyah(this.state.ayahNumberInSurah,this.state.surahNumber)+ ' ' +
+      getAyahNumberInSurah(this.state.ayahNumberInSurah,this.state.surahNumber),
+      surahName: getSurah(this.state.surahNumber)
+    })
+  }
   render() {
-    let { surah, ayahNumber, uthmani } = this.state
+    let { surahName, surahNumber, ayahNumber, uthmani, ayahNumberInSurah } = this.state
     return (
       <div>
-        <Quote
+        <AyahContainer
           text={uthmani}
-          surah={surah}
+          surahName={surahName}
+          surahNumber={surahNumber}
           handleClick={this.handleClick}
           ayahNumber={ayahNumber}
+          ayahNumberInSurah={ayahNumberInSurah}
+          handleChange={this.handleChange}
+          goto={this.goto}
+          handleChangeSurah={this.handleChangeSurah}
         />
       </div>
     )
   }
 }
 
-const Quote = props => {
-  let te = props.text
-    .split('')
-    .map((a,i) => {
-      if(a === 'م' && props.text[i+2] ==='\u0670') {
-        return `<span id=circle>${a}</span>`
-        // (\u0621\u064e\u0627\u0653\S)|(\u0653.\u0651) for openoffice
-        /* Madd6 Muthakkal  98 occurances*/
-      } else if ((a === '\u0627' || a === '\u0648') 
-      && props.text[i+1] === '\u0653' 
-      && props.text[i+3] === '\u0651') {
-        return `<span class=madd6>${a}</span>`
-        /* Madd6 madalfarq 7 occurances */ 
-      } else if (a === 'ا' && props.text[i+1] === '\u0653' &&
-        props.text[i-2] === 'ء' && props.text[i - 1] === '\u064e') {
-        return `<span class=madd6>${a}</span>`
-      } else if ((a === 'ٱ' && props.text[i-1] !== ' '  /* Hamzatalwasl*/
-              && i > 0 && props.text[i+1]+props.text[i+2] !== 'لل') 
-              /* Hamzatalwasl sukun alif*/
-              || (a === '\u0627' && props.text[i+1] === '\u0652') 
-              /* Hamzatalwasl sukun sukun*/
-              || (a === '\u0652' && props.text[i-1] === '\u0627')
-              /* Laam qamar */
-              || (a === 'ل' && props.text[i-1] === 'ٱ' && props.text[i+2] === '\u0651' 
-              && props.text[i]+props.text[i+1] !== 'لل')) {
-        return `<span class=hamzawasl>${a}</span>`
-      }
-      else {
-        return `<span>${a}</span>`
-      }
-    })
-    .join('')
+const AyahContainer = props => {
   return (
     <div id='container'>
  
       <div className="goto">
-        <input type="text" />
+        <form onSubmit={props.goto}>Ayah 
+        
+        <input type='text' className='input' 
+        onChange={props.handleChange}
+        value={props.ayahNumberInSurah}
+        /> 
+        Surah
+        <input type='text' className='input' 
+        onChange={props.handleChangeSurah}
+        value={props.surahNumber}/>
         <button>go to</button>
+        </form>
       </div>
  
-      <p id='surah'>{props.surah}</p>
+      <p id='surah'>{props.surahName}</p>
       {ayahobject[props.ayahNumber].numberInSurah === 1 &&
         ayahobject[props.ayahNumber].surah.number > 1 && (
           <p id='bismillah'>بِسۡمِ ٱللَّهِ ٱلرَّحۡمَٰنِ ٱلرَّحِيمِ</p>
         )}
 
-      <p id='text' dangerouslySetInnerHTML={{ __html: te }} />
+      <p id='text' dangerouslySetInnerHTML={{ __html: tajweed(props.text)} } />
  
       <button id='button' onClick={props.handleClick} className='next-button'>
         آية تالية
@@ -106,3 +115,16 @@ const Quote = props => {
 }
 
 export default App
+
+
+let getAyah = (a,s) => ayahobject.filter(e => {
+  if (e.surah) return e.surah.number == s && e.numberInSurah == a
+})[0].uthmaniText
+
+let getAyahNumberInSurah = (a,s) => ayahobject.filter(e => {
+  if (e.surah) return e.surah.number == s && e.numberInSurah == a
+})[0].numberInSurahNativeReverse
+
+let getSurah = (s) => ayahobject.filter(e => {
+  if (e.surah) return e.surah.number == s
+})[0].surah.name
